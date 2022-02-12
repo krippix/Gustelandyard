0bbcee8b4cd51d92afee8abcf44bc3e3ba82c8ad
krippix <krippix@users.noreply.github.com>
2022-02-13 00:37:23 +0100

    prepared for socket test client

---
+++ client/testclient.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+	//This client ist supposed to test the socket connection to the server
+	//I think later clients will include it in the main application as a choice
+	std::cout << "Damn son, thats a client" << std::endl;
+}
