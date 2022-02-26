e23d0840cdc59e938ca20972b80993fcf3d3d04e
krippix <krippix@users.noreply.github.com>
2022-02-26 03:02:22 +0100

    hacking bullshit together

--- client/testclient.cpp
+++ client/testclient.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <string>
+#include "zmq.hpp"
 
 int main() {
 	//This client ist supposed to test the socket connection to the server
 	//I think later clients will include it in the main application as a choice
 	std::cout << "Damn son, thats a client" << std::endl;
+
+	//initialize zmq context
+	zmq::context_t context{1};
+
+	// construct a request
+	zmq::socket_t socket{ context, zmq::socket_type::req };
+	socket.connect("tcp://localhost:5555");
+
+	// set up some static data
+	const std::string data = "Hello there.";
+
+	for (auto request_num = 0; request_num < 10; ++request_num) {
+		//send the message
+		std::cout << "Sending Hello there" << request_num << "..." << std::endl;
+		socket.send(zmq::buffer(data), zmq::send_flags::none);
+
+		// wait for server reply
+		zmq::message_t reply{};
+		socket.recv(reply, zmq::recv_flags::none);
+
+		std::cout << "Received " << reply.to_string();
+		std::cout << " (" << request_num << ")";
+		std::cout << std::endl;
+	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,38 @@
+#include <thread>
+#include <chrono>
+#include "zmq.hpp"
 #include "player.hpp"
 #include "game.hpp"
 #include "json.hpp"
 
+
 int main() {
+    
+    // initialization
+    zmq::context_t ctx{1};
+    
+    // constructs a reply socket and binds it
+    zmq::socket_t socket{ ctx, zmq::socket_type::rep };
+    socket.bind("tcp://*:5555");
+
+    // prepare static data for responses
+    const std::string data = "Coggers";
+
+    while (true) {
+        zmq::message_t request;
+
+        //recieve request from client
+        socket.recv(request, zmq::recv_flags::none);
+        std::cout << "Poggies: " << request.to_string() << std::endl;
+
+        //sleep
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        //send reply to client
+        socket.send(zmq::buffer(data), zmq::send_flags::none);
+    };
+
+    
     Game currentsession;
 
     //incease turns until game is set to gameover
